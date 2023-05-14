a8056377f557ec073e0a9920fde57b75d83b94c0
garamond13 <98652255+garamond13@users.noreply.github.com>
2023-05-14 12:43:19 +0200

    make macros safer

--- Luma/jincUpscaleLumaUnsharp.glsl
+++ Luma/jincUpscaleLumaUnsharp.glsl
@@ -16,7 +16,7 @@
 ////////////////////////////////////////////////////////////////////////
 
 //based on https://github.com/ImageMagick/ImageMagick/blob/main/MagickCore/enhance.c
-#define sigmoidize(rgba) (M - log(1.0 / ((1.0 / (1.0 + exp(C * (M - 1.0))) - 1.0 / (1.0 + exp(C * M))) * rgba + 1.0 / (1.0 + exp(C * M))) - 1.0) / C)
+#define sigmoidize(rgba) (M - log(1.0 / ((1.0 / (1.0 + exp(C * (M - 1.0))) - 1.0 / (1.0 + exp(C * M))) * (rgba) + 1.0 / (1.0 + exp(C * M))) - 1.0) / C)
 
 vec4 hook() {
     return sigmoidize(clamp(linearize(textureLod(HOOKED_raw, HOOKED_pos, 0.0) * HOOKED_mul), 0.0, 1.0));
@@ -64,38 +64,39 @@ vec4 hook() {
 #define M_PI 3.14159265358979323846
 #define M_PI_2 1.57079632679489661923
 #define M_PI_4 0.785398163397448309616
+#define M_2_PI 0.636619772367581343076
 #define EPSILON 1.192093e-7
 
-#define J1(x) (x < 2.293115733230086 ? (x / 2.0) - (x * x * x / 16.0) + (x * x * x * x * x / 384.0) - (x * x * x * x * x * x * x / 18432.0) : sqrt(2.0 / (M_PI * x)) * (1.0 + 3.0 / (16.0 * x * x) - 99.0 / (512.0 * x * x * x * x)) * cos(x - 3.0 * M_PI_4 + 3.0 / (8.0 * x) - 21.0 / (128.0 * x * x * x)))
+#define J1(x) ((x) < 2.293115733230086 ? ((x) / 2.0) - ((x) * (x) * (x) / 16.0) + ((x) * (x) * (x) * (x) * (x) / 384.0) - ((x) * (x) * (x) * (x) * (x) * (x) * (x) / 18432.0) : sqrt(M_2_PI / (x)) * (1.0 + 3.0 / 16.0 / ((x) * (x)) - 99.0 / 512.0 / ((x) * (x) * (x) * (x))) * cos((x) - 3.0 * M_PI_4 + 3.0 / 8.0 / (x) - 21.0 / 128.0 / ((x) * (x) * (x))))
 
-#define jinc(x) (x < EPSILON ? M_PI_2 : J1(M_PI / B * x) * B / x)
+#define jinc(x) ((x) < EPSILON ? M_PI_2 : J1(M_PI / B * (x)) * B / (x))
 
 #if K == GINSENG
-    #define k(x) (jinc(x) * (x < EPSILON ? M_PI : sin(M_PI / R * x) * R / x))
+    #define k(x) (jinc(x) * ((x) < EPSILON ? M_PI : sin(M_PI / R * (x)) * R / (x)))
 #elif K == COSINE
-    #define k(x) (jinc(x) * pow(cos(M_PI_2 / R * x), P1))
+    #define k(x) (jinc(x) * pow(cos(M_PI_2 / R * (x)), P1))
 #elif K == GARAMOND
-    #define k(x) (jinc(x) * pow(1.0 - pow(x / R, P1), P2))
+    #define k(x) (jinc(x) * pow(1.0 - pow((x) / R, P1), P2))
 #elif K == BLACKMAN
-    #define k(x) (jinc(x) * pow((1.0 - P1) / 2.0 + 0.5 * cos(M_PI / R * x) + P1 / 2.0 * cos(2.0 * M_PI / R * x), P2))
+    #define k(x) (jinc(x) * pow((1.0 - P1) / 2.0 + 0.5 * cos(M_PI / R * (x)) + P1 / 2.0 * cos(2.0 * M_PI / R * (x)), P2))
 #elif K == GNW
-    #define k(x) (jinc(x) * exp(-pow(x / P1, P2)))
+    #define k(x) (jinc(x) * exp(-pow((x) / P1, P2)))
 #elif K == SAID
-    #define k(x) (jinc(x) * cosh(sqrt(2.0 * P2) * M_PI * P1 / (2.0 - P2) * x) * exp(-M_PI * M_PI * P1 * P1 / ((2.0 - P2) * (2.0 - P2)) * x * x))
+    #define k(x) (jinc(x) * cosh(sqrt(2.0 * P2) * M_PI * P1 / (2.0 - P2) * (x)) * exp(-M_PI * M_PI * P1 * P1 / ((2.0 - P2) * (2.0 - P2)) * (x) * (x)))
 #elif K == FSR
     #undef R
     #define R 2.0
-    #define k(x) ((1.0 / (2.0 * P1 - P1 * P1) * (P1 / (P2 * P2) * x * x - 1.0) * (P1 / (P2 * P2) * x * x - 1.0) - (1.0 / (2.0 * P1 - P1 * P1) - 1.0)) * (0.25 * x * x - 1.0) * (0.25 * x * x - 1.0))
+    #define k(x) ((1.0 / (2.0 * P1 - P1 * P1) * (P1 / (P2 * P2) * (x) * (x) - 1.0) * (P1 / (P2 * P2) * (x) * (x) - 1.0) - (1.0 / (2.0 * P1 - P1 * P1) - 1.0)) * (0.25 * (x) * (x) - 1.0) * (0.25 * (x) * (x) - 1.0))
 #elif K == BCSPLINE
     #undef R
     #define R 2.0
-    #define k(x) (x < 1.0 ? (12.0 - 9.0 * P1 - 6.0 * P2) * x * x * x + (-18.0 + 12.0 * P1 + 6.0 * P2) * x * x + (6.0 - 2.0 * P1) : (-P1 - 6.0 * P2) * x * x * x + (6.0 * P1 + 30.0 * P2) * x * x + (-12.0 * P1 - 48.0 * P2) * x + (8.0 * P1 + 24.0 * P2))
+    #define k(x) ((x) < 1.0 ? (12.0 - 9.0 * P1 - 6.0 * P2) * (x) * (x) * (x) + (-18.0 + 12.0 * P1 + 6.0 * P2) * (x) * (x) + (6.0 - 2.0 * P1) : (-P1 - 6.0 * P2) * (x) * (x) * (x) + (6.0 * P1 + 30.0 * P2) * (x) * (x) + (-12.0 * P1 - 48.0 * P2) * (x) + (8.0 * P1 + 24.0 * P2))
 #endif
 
-#define get_weight(x) (x < R ? k(x) : 0.0)
+#define get_weight(x) ((x) < R ? k(x) : 0.0)
 
 //based on https://github.com/ImageMagick/ImageMagick/blob/main/MagickCore/enhance.c
-#define desigmoidize(rgba) (1.0 / (1.0 + exp(C * (M - rgba))) - 1.0 / (1.0 + exp(C * M))) / ( 1.0 / (1.0 + exp(C * (M - 1.0))) - 1.0 / (1.0 + exp(C * M)))
+#define desigmoidize(rgba) (1.0 / (1.0 + exp(C * (M - (rgba)))) - 1.0 / (1.0 + exp(C * M))) / ( 1.0 / (1.0 + exp(C * (M - 1.0))) - 1.0 / (1.0 + exp(C * M)))
 
 vec4 hook() {
     vec2 fcoord = fract(PASS1_pos * input_size - 0.5);
@@ -142,7 +143,7 @@ vec4 hook() {
 //
 ////////////////////////////////////////////////////////////////////////
 
-#define get_weight(x) (exp(-x * x / (2.0 * S * S)))
+#define get_weight(x) (exp(-(x) * (x) / (2.0 * S * S)))
 
 vec4 hook() {
     float weight;
@@ -177,7 +178,7 @@ vec4 hook() {
 //
 ////////////////////////////////////////////////////////////////////////
 
-#define get_weight(x) (exp(-x * x / (2.0 * S * S)))
+#define get_weight(x) (exp(-(x) * (x) / (2.0 * S * S)))
 
 vec4 hook() {
     float weight;
